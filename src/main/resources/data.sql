55515edd73129d0d8030702cc4e1013c0cb11ca7
kao <ammaret.a@appman.co.th>
2023-01-03 16:45:51 +0700

    edit-initial-not-delete-table

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,20 +1,14 @@
 INSERT INTO test_user(first_name,last_name,email_user,password)
-    VALUES ('dodo','love','dodo@gmail.com','4321'),
-           ('hug','vow','hug@gmail.com','1234');
-
-INSERT INTO cat (name,age_year,age_month,weight,gender,id_u,id_food)
-    VALUES ('casuya',1,2,2,'male',1,1),
-           ('preto',3,6,5,'male',2,2);
+    VALUES ('dodo','love','dodo@gmail.com','4321') ON CONFLICT (email_user) do nothing;
+INSERT INTO test_user(first_name,last_name,email_user,password)
+    VALUES ('hug','vow','hug@gmail.com','1234') ON CONFLICT (email_user) do nothing;
 
 INSERT INTO brand (description,brand,cal)
-    VALUES ('food for cat','nahug food',365),
-           ('good cat food','meo',360);
+    VALUES ('food for cat','nahug food',365) ON CONFLICT (brand) do nothing;
+INSERT INTO brand (description,brand,cal)
+    VALUES ('good cat food','meo',360) ON CONFLICT (brand) do nothing;
 
 INSERT INTO vaccine (name_vaccine)
-    VALUES ('FPV'),
-           ('Rabbit');
-
-INSERT INTO phase (age_month,next_time,count_vaccine,vaccine_id,phase)
-    VALUES (2,1,3,1,1),
-           (2,12,0,1,2),
-           (12,12,0,2,1);
+    VALUES ('FPV') on CONFLICT (name_vaccine) do nothing;
+INSERT INTO vaccine (name_vaccine)
+    VALUES ('Rabbit') on CONFLICT (name_vaccine) do nothing;
